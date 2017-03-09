2e751bf2b2a350a0ecc0012b7af2285e81a22142
David Martinez <davidmgilo@gmail.com>
2017-03-09 18:23:11 +0100

    bounding

--- src/functions/move.lua
+++ src/functions/move.lua
@@ -1,8 +1,12 @@
 function move(dt)
     if love.keyboard.isDown('left','a') then
-        player.x = player.x - (player.speed*dt)
+        if player.x > 0 then -- binds us to the map
+            player.x = player.x - (player.speed*dt)
+        end
     elseif love.keyboard.isDown('right','d') then
-        player.x = player.x + (player.speed*dt)
+        if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
+            player.x = player.x + (player.speed*dt)
+        end
     end
 end
 
